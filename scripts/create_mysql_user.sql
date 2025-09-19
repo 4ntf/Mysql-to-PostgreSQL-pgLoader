5e673752ff0b4fe0d9728f7df3f41697804ee715
4ntf <anim8683@gmail.com>
2025-09-19 15:49:05 +0200

    Update create_mysql_user.sql

--- scripts/create_mysql_user.sql
+++ scripts/create_mysql_user.sql
@@ -1,6 +1,4 @@
-```sql
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'nuova_password';
 CREATE USER 'massimo'@'%' IDENTIFIED BY 'admin123';
 GRANT ALL PRIVILEGES ON prova_db.* TO 'massimo'@'%';
 FLUSH PRIVILEGES;
-```
